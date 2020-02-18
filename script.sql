b228c84dc5ebaf15088852e1df7e38d4d1369ee7
Tyler Kazar <tk322@njit.edu>
2020-02-18 12:51:56 -0500

    Added tables for portfolios, and stock data.

--- script.sql
+++ script.sql
@@ -1,7 +1,9 @@
-CREATE DATABASE SiteDirectory;
+CREATE DATABASE TestDirectory;
+
+USE TestDirectory;
 
 CREATE TABLE SiteUsers(
-	id INT NOT NULL KEY AUTO_INCREMENT,
+	user_id INT NOT NULL KEY AUTO_INCREMENT,
 	username VARCHAR(255) NOT NULL,
 	password VARCHAR(255) NOT NULL,
 	email VARCHAR(255) NOT NULL,
@@ -10,16 +12,52 @@ CREATE TABLE SiteUsers(
 	date_joined DATETIME
 );
 
-CREATE TABLE StockSymbols(
+CREATE TABLE StockInfo(
 	stock_sym varchar(20) NOT NULL KEY,
-	stock_name varchar(255),
+	stock_name varchar(255)
 );
 
-CREATE TABLE PORTFOLIO(
-PORTFOLIO VARCHAR(41) NOT NULL PRIMARY KEY,
-BALANCE MONEY NOT NULL,
-
-USERNAME VARCHAR(41),
-CONSTRAINT C_FK_1 FOREIGN KEY (USERNAME) REFERENCES USERS (USERNAME)
-
+CREATE TABLE StockData(
+	stock_sym VARCHAR(20) NOT NULL,
+	price_time ENUM('O', 'C') NOT NULL,
+	price FLOAT NOT NULL,
+	volume INT NOT NULL,
+	timestamp TIMESTAMP NOT NULL,
+	PRIMARY KEY(stock_sym, timestamp),
+	FOREIGN KEY (stock_sym) REFERENCES StockInfo(stock_sym)
 );
+
+CREATE TABLE UserPortfolio(
+	portfolio_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	user_id INT NOT NULL,
+	stock_sym VARCHAR(20) NULL,
+	stock_volume INT NULL,
+	stock_value FLOAT NULL,
+	FOREIGN KEY (user_id) REFERENCES SiteUsers(user_id),
+	FOREIGN KEY (stock_sym) REFERENCES StockData(stock_sym)
+);
+
+CREATE TABLE transactions(
+	trans_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	user_id INT NOT NULL,
+	price_type ENUM('S', 'B') NOT NULL,
+	stock_sym VARCHAR(20) NOT NULL,
+	value FLOAT NOT NULL,
+	trans_volume INT NOT NULL, 
+	FOREIGN KEY (user_id) REFERENCES SiteUsers(user_id),
+	FOREIGN KEY (stock_sym) REFERENCES StockData(stock_sym)
+);
+
+CREATE TABLE userAccount(
+	user_id INT NOT NULL,
+	portfolio_id INT NULL,
+	portfolio_value FLOAT NOT NULL DEFAULT 100000,
+	FOREIGN KEY (portfolio_id) REFERENCES UserPortfolio(portfolio_id)
+);
+
+
+CREATE USER 'connectionuser'@'localhost' IDENTIFIED BY 'conn3ctpass';
+GRANT ALL ON *.* TO 'connectionuser'@'localhost' WITH GRANT OPTION;
+
+
+
