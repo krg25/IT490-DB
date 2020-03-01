d83b69208276f21d918c865efaaad1036015afb1
Kenneth <krg25@njit.edu>
2020-03-01 16:14:53 -0500

    Forgot to change dependency  in mysqli_connect

--- mysqli_connect.php
+++ mysqli_connect.php
@@ -1,7 +1,7 @@
 <?php 
 require_once('Logger.php.inc');
 
-$log = new Logger('mysqli_connect','rabbit.ini');
+$log = new Logger('mysqli_connect','rabbit/rabbit.ini');
 
 
 $dbc = @mysqli_connect('localhost', 'connectionuser', 'conn3ctpass', 'StockApp') OR die ($log->logError("Connection Failure","ERROR","Failed to connect: ".mysqli_connect_error()));
